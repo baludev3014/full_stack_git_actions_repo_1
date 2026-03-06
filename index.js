0df921a5da9bb9f22f99be4da594665421fa69ce
baludev3014 <kbalu3013@gmail.com>
2026-03-06 14:09:29 +0530

    new route wad added to index.js file

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/',(req,res)=>{
     res.send("Home route")
 })
 
+app.get('/orders',(req,res)=>{
+    res.send("orders")
+})
+
 app.listen(4000,()=>{
     console.log("App is listening on port 4000")
 })
